11afb7970a16500e7c4d6f99e4ef306ec1c92adf
Alvyn Wang <alvyn.wang@gmail.com>
2017-03-25 19:05:55 -0700

    timer

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() { 
 	$('div').addClass('red');
+	var timer = setInterval(function() {
+
+    var count = parseInt($('#clock').html());
+    if (count !== 0) {
+      $('#clock').html(count - 1);
+    } else {
+      clearInterval(timer);
+      $('#clock').html("Time's Up");
+    }
+  }, 1000);
+	$('div').removeClass('red');
 });
