4eba5f0c5c390a6fa1b19e12f5f00e79035c9937
Alvyn Wang <alvyn.wang@gmail.com>
2017-03-25 22:17:21 -0700

    adding to timer

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 $(document).ready(function() { 
+	var count = parseInt($('#clock').html());
+	function newTile (tile) {
+		var x = Math.floor(Math.random() * 3);
+    	if (x == 0) {
+    		$(tile).removeClass('blue').removeClass('yellow').addClass('red');
+    	} else if (x == 1) {
+    		$(tile).removeClass('yellow').removeClass('red').addClass('blue');
+    	} else if (x == 2) {
+    		$(tile).removeClass('red').removeClass('blue').addClass('yellow');
+    	}
+    	//$(asdf2).html(x);
+	}
+
+	function checkIfAlive () {
+		if (count > 0) {
+	      count = (count - 1);
+	      $('#clock').html(count);
+	    } else {
+	      clearInterval(timer);
+	      $('#clock').html("Time's Up");
+	    }
+	}
 	
 	var timer = setInterval(function() {
 
 		var time = 0;
-	    var count = parseInt($('#clock').html());
-	    if (count !== 0) {
-	      $('#clock').html(count - 1);
+	    //count = parseInt($('#clock').html());
+	    if (count > 0) {
+	      count = (count - 1);
+	      $('#clock').html(count);
 	    } else {
 	      clearInterval(timer);
 	      $('#clock').html("Time's Up");
 	    }
 	    for (var i = 0; i < 9; i++) {
-	    	var x = Math.floor(Math.random() * 3);
 	    	var asdf = i;
 	    	var asdfg = '#';
 	    	var asdf2 = asdfg.concat(asdf, "");
-    		if (x == 0) {
-    			$(asdf2).removeClass('blue').removeClass('yellow').addClass('red');
-    		}
-    		else if (x == 1) {
-    			$(asdf2).removeClass('yellow').removeClass('red').addClass('blue');
-    		}
-    		else if (x == 2)
-    		{
-    			$(asdf2).removeClass('red').removeClass('blue').addClass('yellow');
-    		}
-    		 $(asdf2).html(x);
+	    	newTile(asdf2);
     	}
   	}, 1000);
+  	$('div').click(function () {
+  		var color = $(this);
+  		if (color.hasClass('red')) {
+  			count -= 10;
+  		} else if (color.hasClass('yellow')) {
+  			count += 3;
+  		} else if (color.hasClass('blue')) {
+  			score += 10;
+  			updateScore();
+  		}
+  		newTile(color);
+  		checkIfAlive();
+  	});
 });
