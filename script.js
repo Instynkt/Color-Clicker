bd96c81907207a99e5031345738336af25b223b3
Alvyn Wang <alvyn.wang@gmail.com>
2017-03-25 21:26:48 -0700

    colors change

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 $(document).ready(function() { 
-	$('div').addClass('red');
+	
 	var timer = setInterval(function() {
 
-    var count = parseInt($('#clock').html());
-    if (count !== 0) {
-      $('#clock').html(count - 1);
-    } else {
-      clearInterval(timer);
-      $('#clock').html("Time's Up");
-    }
-  }, 1000);
-	$('div').removeClass('red');
+		var time = 0;
+	    var count = parseInt($('#clock').html());
+	    if (count !== 0) {
+	      $('#clock').html(count - 1);
+	    } else {
+	      clearInterval(timer);
+	      $('#clock').html("Time's Up");
+	    }
+	    for (var i = 0; i < 9; i++) {
+	    	var x = Math.floor(Math.random() * 3);
+	    	var asdf = i;
+	    	var asdfg = '#';
+	    	var asdf2 = asdfg.concat(asdf, "");
+    		if (x == 0) {
+    			$(asdf2).removeClass('blue').removeClass('yellow').addClass('red');
+    		}
+    		else if (x == 1) {
+    			$(asdf2).removeClass('yellow').removeClass('red').addClass('blue');
+    		}
+    		else if (x == 2)
+    		{
+    			$(asdf2).removeClass('red').removeClass('blue').addClass('yellow');
+    		}
+    		 $(asdf2).html(x);
+    	}
+  	}, 1000);
 });
